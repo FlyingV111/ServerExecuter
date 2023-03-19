06c63d2fdeb9809c6980bb4df691bc4be26ac924
FlyingV111 <73441162+FlyingV111@users.noreply.github.com>
2023-03-19 22:11:16 +0100

    Update

--- ServerExecuter/StartServer.ps1
+++ ServerExecuter/StartServer.ps1
@@ -1,26 +1,23 @@
+cls
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
+Add-Type @"
+using System.Windows.Forms;
+public class objForm : Form {
+    public string ServerPath { get; set; }
+}
+"@
+
 $iconPath = ".\server.ico"
 $folderPath = "Logo"
+$objForm.serverPath = ""
+$batName = ""
 
 $objForm = New-Object System.Windows.Forms.Form
 $objForm.Text = "Server-Management"
 $objForm.StartPosition = "CenterScreen"
 $objForm.Size = New-Object System.Drawing.Size(800,500)
-
-$drives = (Get-PSDrive -PSProvider FileSystem).Name
-
-$objListbox = New-Object System.Windows.Forms.Listbox 
-$objListbox.Location = New-Object System.Drawing.Size(10,40) 
-$objListbox.Size = New-Object System.Drawing.Size(20,20) 
-$objListbox.SelectionMode = "MultiExtended"
-$objListbox.Height = 70
-$psDrivers = Get-PrinterDriver | Where-Object {$_.Type -eq "PS"}
-foreach ($psDriver in $psDrivers) {
-    $objListBox.Items.Add($psDriver.Name)
-}
-$objForm.Controls.Add($objListbox) 
 
 $objLabel = New-Object System.Windows.Forms.Label
 $objLabel.Location = New-Object System.Drawing.Size(20,20)
@@ -28,22 +25,77 @@ $objLabel.Size = New-Object System.Drawing.Size(200,20)
 $objLabel.Text = "Server-Ordner Path :"
 $objForm.Controls.Add($objLabel)
 
-$objTextBox = New-Object System.Windows.Forms.TextBox
-$objTextBox.Location = New-Object System.Drawing.Size(20,40)
-$objTextBox.Size = New-Object System.Drawing.Size(200,20)
-$objForm.Controls.Add($objTextBox)
+$textBoxSelect = New-Object System.Windows.Forms.TextBox
+$textBoxSelect.Location = New-Object System.Drawing.Size(20,40)
+$textBoxSelect.Size = New-Object System.Drawing.Size(200,20)
+$objForm.Controls.Add($textBoxSelect)
 
-$ApplyButton = New-Object System.Windows.Forms.Button
-# Die nächsten beiden Zeilen legen die Position und die Größe des Buttons fest
-$ApplyButton.Location = New-Object System.Drawing.Size(240,39)
-$ApplyButton.Size = New-Object System.Drawing.Size(75,23)
-$ApplyButton.Text = "Apply"
-$ApplyButton.Name = "Apply"
-#Die folgende Zeile ordnet dem Click-Event die Schließen-Funktion für das Formular zu
-$ApplyButton.Add_Click({
-    Get-PSDrive
+$selectPath = New-Object System.Windows.Forms.Button
+$selectPath.Location = New-Object System.Drawing.Size(225,39)
+$selectPath.Size = New-Object System.Drawing.Size(30,22)
+$selectPath.Text = "..."
+$selectPath.Add_Click({
+    $folderBrowser =  New-Object System.Windows.Forms.FolderBrowserDialog
+    if ($folderBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::Ok) {
+        $textBoxSelect.Text = $folderBrowser.SelectedPath
+    }
+
 })
-$objForm.Controls.Add($ApplyButton)
+$objForm.Controls.Add($selectPath)
+
+$ApplyButtonPath = New-Object System.Windows.Forms.Button
+# Die nächsten beiden Zeilen legen die Position und die Größe des Buttons fest
+$ApplyButtonPath.Location = New-Object System.Drawing.Size(280,39)
+$ApplyButtonPath.Size = New-Object System.Drawing.Size(75,23)
+$ApplyButtonPath.Text = "Apply"
+$ApplyButtonPath.Name = "ApplyButtonPath"
+#Die folgende Zeile ordnet dem Click-Event die Schließen-Funktion für das Formular zu
+$ApplyButtonPath.Add_Click({
+    Write-Host "Server Path:"
+    $objForm.serverPath = $textBoxSelect.Text
+    Write-Host $objForm.serverPath
+})
+$objForm.Controls.Add($ApplyButtonPath)
+
+
+$objLabel = New-Object System.Windows.Forms.Label
+$objLabel.Location = New-Object System.Drawing.Size(20,79)
+$objLabel.Size = New-Object System.Drawing.Size(200,20)
+$objLabel.Text = "Server-Run Bat:"
+$objForm.Controls.Add($objLabel)
+
+$textBoxBat = New-Object System.Windows.Forms.TextBox
+$textBoxBat.Location = New-Object System.Drawing.Size(20,99)
+$textBoxBat.Size = New-Object System.Drawing.Size(200,20)
+$objForm.Controls.Add($textBoxBat)
+
+$selectPathBat = New-Object System.Windows.Forms.Button
+$selectPathBat.Location = New-Object System.Drawing.Size(225,99)
+$selectPathBat.Size = New-Object System.Drawing.Size(30,22)
+$selectPathBat.Text = "..."
+$selectPathBat.Add_Click({
+    $batBrowser =  New-Object System.Windows.Forms.OpenFileDialog
+    $batBrowser.Filter = "Batch Files (*.bat, *.cmd)|*.bat; *.cmd"
+    if ($batBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::Ok) {
+        $textBoxBat.Text = [System.IO.Path]::GetFileName($batBrowser.FileName)
+    }
+
+})
+$objForm.Controls.Add($selectPathBat)
+
+$ApplyButtonBat = New-Object System.Windows.Forms.Button
+# Die nächsten beiden Zeilen legen die Position und die Größe des Buttons fest
+$ApplyButtonBat.Location = New-Object System.Drawing.Size(280,99)
+$ApplyButtonBat.Size = New-Object System.Drawing.Size(75,23)
+$ApplyButtonBat.Text = "Apply"
+$ApplyButtonBat.Name = "Apply"
+#Die folgende Zeile ordnet dem Click-Event die Schließen-Funktion für das Formular zu
+$ApplyButtonBat.Add_Click({
+    Write-Host "Bat Name:"
+    global:batName = $textBoxBat.Text
+    Write-Host $batName
+})
+$objForm.Controls.Add($ApplyButtonBat)
 
 $CancelButton = New-Object System.Windows.Forms.Button
 # Die nächsten beiden Zeilen legen die Position und die Größe des Buttons fest
@@ -64,30 +116,36 @@ $StartButton.Text = "Start"
 $StartButton.Name = "Start"
 #Die folgende Zeile ordnet dem Click-Event die Schließen-Funktion für das Formular zu
 $StartButton.Add_Click({
-
+    Write-Host "Path:"
+    Write-Host $objForm.serverPath
+    startServer
+    startNgrok
 })
 $objForm.Controls.Add($StartButton)
 
-#[void] $objForm.ShowDialog()
+[void] $objForm.ShowDialog()
 
 function startServer{
+    $objForm.serverPath
+    $batName
+
     #Start Bat 
-    ServerSet-Location $serverPath
-    Start-Process run.bat
+    Set-Location $objForm.serverPath
+    Start-Process $batName
 }
-$serverPath = "E:\Order\AllServer\Minecraft-Server\TestServer"
-$batPath = "E:\Order\AllServer\Minecraft-Server\TestServer\run.bat"
 
+function startNgrok{
+$ngrokJob = Start-Job -ScriptBlock {"start $Using:serverPath\ngrok.exe tcp 25565 --region eu" |  cmd}
 
-
-$ngrokJob = Start-Job -ScriptBlock {"start / $Using:serverPath\ngrok.exe tcp 25565 --region eu" |  cmd}
 sleep 2
 
 $url = (Invoke-WebRequest -UseBasicParsing -uri "http://localhost:4040/api/tunnels").Content
+echo $url
 $Json= ConvertFrom-Json -InputObject $url
-$url = $contentJson.tunnels.public_url
+$url = $Json.tunnels.public_url
 $url = $url.trimStart("tcp://")
 $url
 
 Stop-Job -id $ngrokJob.Id
 Remove-Job -id $ngrokJob.Id
+}
