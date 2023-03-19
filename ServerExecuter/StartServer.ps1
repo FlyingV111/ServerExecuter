1855775ec1855682aa29c1070391cf28cfd36b57
FlyingV111 <73441162+FlyingV111@users.noreply.github.com>
2023-03-19 15:20:18 +0100

    Working

--- ServerExecuter/StartServer.ps1
+++ ServerExecuter/StartServer.ps1
@@ -1,23 +1,25 @@
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
+$iconPath = ".\server.ico"
+$folderPath = "Logo"
+
 $objForm = New-Object System.Windows.Forms.Form
 $objForm.Text = "Server-Management"
 $objForm.StartPosition = "CenterScreen"
-$objForm.Icon="E:\Order\AllServer\ServerExecuter\Logo\server.ico"
 $objForm.Size = New-Object System.Drawing.Size(800,500)
 
 $drives = (Get-PSDrive -PSProvider FileSystem).Name
 
-foreach(%drives%){
-    echo Hallo
-}
-
 $objListbox = New-Object System.Windows.Forms.Listbox 
 $objListbox.Location = New-Object System.Drawing.Size(10,40) 
-$objListbox.Size = New-Object System.Drawing.Size(260,20) 
+$objListbox.Size = New-Object System.Drawing.Size(20,20) 
 $objListbox.SelectionMode = "MultiExtended"
 $objListbox.Height = 70
+$psDrivers = Get-PrinterDriver | Where-Object {$_.Type -eq "PS"}
+foreach ($psDriver in $psDrivers) {
+    $objListBox.Items.Add($psDriver.Name)
+}
 $objForm.Controls.Add($objListbox) 
 
 $objLabel = New-Object System.Windows.Forms.Label
@@ -64,16 +66,17 @@ $StartButton.Name = "Start"
 $StartButton.Add_Click({
     cd "E:\Order\AllServer\Minecraft Server\TestServer"
     start run.bat
-    E:
-    cd "E:\Order\AllServer\Minecraft Server"  
-    #.\ngrok.exe tcp 25565 --region eu --log=stdout > .\Log.txt 2>&1;
 
-    echo Moin
+
+
     $extract = Select-String -Path "Log.txt" -Pattern "url=tcp:";
     echo $extract
     pause
 })
 $objForm.Controls.Add($StartButton)
 
-[void] $objForm.ShowDialog()
+#[void] $objForm.ShowDialog()
 
+E:
+cd "E:\Order\AllServer\Minecraft-Server"  
+.\ngrok.exe tcp 25565 --region eu --log=stdout > .\Log.txt 2>&1;
